80f8422ed6e20d018773c4efb1e2ea00bef36b40
Aron Ahmadia <aron@ahmadia.net>
2013-07-02 21:38:28 -0400

    reformatted boundary_conditions.h

--- src/cudaclaw/cuda/boundary_conditions.h
+++ src/cudaclaw/cuda/boundary_conditions.h
@@ -1,4 +1,4 @@
-#ifndef __BOUNDARY_CONDITIONS_H__ 
+#ifndef __BOUNDARY_CONDITIONS_H__
 #define __BOUNDARY_CONDITIONS_H__
 
 #include "boundary_conditions_header.h"
@@ -6,25 +6,25 @@
 template<class BCS>
 __global__ void boundary_kernel(pdeParam param, BCS conditions)
 {
-	int thread_global_id = threadIdx.x + blockDim.x*blockIdx.x;
-	conditions.condition_left(param, thread_global_id);
-	conditions.condition_right(param, thread_global_id);
-	conditions.condition_up(param, thread_global_id);
-	conditions.condition_down(param, thread_global_id);
+    int thread_global_id = threadIdx.x + blockDim.x*blockIdx.x;
+    conditions.condition_left(param, thread_global_id);
+    conditions.condition_right(param, thread_global_id);
+    conditions.condition_up(param, thread_global_id);
+    conditions.condition_down(param, thread_global_id);
 }
 
 template<class BCS>
 void setBoundaryConditions(pdeParam& param, BCS bcs)
 {
-	unsigned int blockDimensionX = 256; 
+    unsigned int blockDimensionX = 256;
 
-	int maxSide = (param.cellsX >= param.cellsY)? param.cellsX:param.cellsY;
-	unsigned int gridDimensionX = (maxSide+blockDimensionX-1)/blockDimensionX;
+    int maxSide = (param.cellsX >= param.cellsY)? param.cellsX:param.cellsY;
+    unsigned int gridDimensionX = (maxSide+blockDimensionX-1)/blockDimensionX;
 
-	dim3 dimGrid(gridDimensionX);
-	dim3 dimBlock(blockDimensionX);
+    dim3 dimGrid(gridDimensionX);
+    dim3 dimBlock(blockDimensionX);
 
-	boundary_kernel<<<dimGrid, dimBlock>>>(param, bcs);
+    boundary_kernel<<<dimGrid, dimBlock>>>(param, bcs);
 }
 
 #endif
