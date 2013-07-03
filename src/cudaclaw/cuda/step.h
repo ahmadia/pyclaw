01e042c70da4832ff595e5bec938e4482bba46cc
Aron Ahmadia <aron@ahmadia.net>
2013-07-02 22:49:48 -0400

    step.h reformatting

--- src/cudaclaw/cuda/step.h
+++ src/cudaclaw/cuda/step.h
@@ -4,25 +4,22 @@
 #include "boundary_conditions.h"
 #include "fused_Riemann_Limiter.h"
 
-template <class Riemann_h, class Riemann_v, class Limiter, class BCS>
-    real step	(pdeParam &param, // Problem parameters
-		 Riemann_h Riemann_pointwise_solver_h,	// Riemann problem solver for vertical interfaces, device function
-		 Riemann_v Riemann_pointwise_solver_v,	// Riemann problem solver for horizontal interfaces device function
-		 Limiter limiter_phi, // limiter device function called from a fused Riemann-Limiter function,
-		 // takes the Riemann solver as parameter along other parameters (waves, and others?)
-		 BCS boundary_conditions) // Boundary conditions put in one object
+template <class Riemann_h, class Riemann_v, class Limiter, class BCS, class Ent_h, class Ent_v>
+    real step(pdeParam &param,			        // Problem parameters
+	      Riemann_h Riemann_pointwise_solver_h,	// Riemann problem solver for vertical interfaces, device function
+	      Riemann_v Riemann_pointwise_solver_v,	// Riemann problem solver for horizontal interfaces device function
+	      Limiter limiter_phi,			// limiter device function called from a fused Riemann-Limiter function,
+		                                        // takes the Riemann solver as parameter along other parameters (waves, and others?)
+	      BCS boundary_conditions,		        // Boundary conditions put in one object
+	      Ent_h entropy_fix_h,		        // Entropy fix for horizontal
+	      Ent_v entropy_fix_v)			// Entropy fix for vertical
 
 {
-    //static real dt = (real)0.0001f; /*/ param.dx/5;	//	awesome failing effects!!/*/
     real dt;
 
     setBoundaryConditions(param, boundary_conditions);
-
-    limited_Riemann_Update(param, Riemann_pointwise_solver_h, Riemann_pointwise_solver_v, limiter_phi);
-
+    limited_Riemann_Update(param, Riemann_pointwise_solver_h, Riemann_pointwise_solver_v, limiter_phi, entropy_fix_h, entropy_fix_v);
     cudaMemcpy(&dt, param.dt_used, sizeof(real), cudaMemcpyDeviceToHost);
-
-    printf("time step dt: %f\n", dt);
 
     return dt;
 }
